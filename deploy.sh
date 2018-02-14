62b605674b0d6f3b2565ba210ef40a65b569aa85
hoge@fuga.email <hoge>
2018-02-14 15:11:07 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd `dirname $0`
+for f in ??*
+do
+    case $f in
+      .git) continue;;
+      .gitignore) continue;;
+      .DS_Store) continue;;
+      README.md) continue;;
+      $0) continue;;
+    esac
+    [ -f ~/$f ] && continue || echo create symlink $f
+
+    ln -sv $(pwd)/$f ~/.w3m
+done
