a55357931d5b00e23b0366c984506287abafbddf
user <>
2018-04-19 13:30:57 +0900

    sample cgi to load page in external browser

---
+++ cgi-bin/extern.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# for macOS High Sierra, no for other platforms
+# file:/cgi-bin/extern.sh
+# only chrome is working properly. Sarari/Firefox are useless
+# Preferences > External browser = /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "$W3M_URL"
+
